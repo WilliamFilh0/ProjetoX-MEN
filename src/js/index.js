f790369bdd2e982873a8c0565965511203422c33
WilliamFilh0 <williamfilho8908@gmail.com>
2023-07-20 02:14:32 -0300

    Personagem selecionado

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+const personagens = document.querySelectorAll('.personagem');
+//personagem selecionado
+personagens.forEach((personagem) => {
+  personagem.addEventListener('mouseenter', () => {
+    const personagemSelecionado = document.querySelector('.selecionado');
+   
+  });
+});
